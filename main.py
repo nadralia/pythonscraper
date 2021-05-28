9a52449456174cf4234cabba17d106ddea52990a
Adralia Nelson <nadralia@gmail.com>
2021-05-28 10:35:55 +0300

    csv logic

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 
-source = requests.get('https://www.doordash.com/store/yafo-kitchen-charlotte-63003')
+URL = 'https://www.doordash.com/store/yafo-kitchen-charlotte-63003'
 
-soup = BeautifulSoup(source.content, 'html.parser')
-print(soup)
+source = requests.get(URL).text
+
+soup = BeautifulSoup(source, 'html5lib') 
+
+
+csv_file = open('web_scraper_data.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(["Category", "Item Name", "Description Price",	"Option Name", "Choice", "Choice Price"])
+
+
+for menus in soup.find_all('h1'):
+    csv_writer.writerow([menus])
+    print(menus)
+
+# print(soup.prettify())
