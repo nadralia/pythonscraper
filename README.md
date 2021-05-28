1f63aa99873eb4f218ade1e041c18281fae0b2b9
Adralia Nelson <nadralia@gmail.com>
2021-05-28 10:41:38 +0300

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # pythonscraper
+
+**Getting started with the app**
+
+### Technologies used to build the application and make sure  you have them
+- `Python3` - A programming language that lets us work more quickly.
+- `Virtualenv` - A tool to create an isolated virtual environment.
+- `Git` - Version Control System for tracking your changes.
+### Installation
+
+Clone this repository by running
+
+```sh
+git clone git@github.com:nadralia/pythonscraper.git
+```
+
+Cd to pythonscraper and Create a virtual environment. For example, with virtualenv, create a virtual environment named venv using
+
+```sh
+virtualenv venv
+```
+
+Activate the virtual environment
+
+```sh
+source venv/bin/activate
+```
+
+Install the dependencies in the requirements.txt file using pip
+
+```sh
+pip install -r requirements.txt
+```
+
+
+Run the app
+
+```sh
+python main.py
+```
--- main.py
+++ main.py
@@ -2,8 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
-
+BASE_URL = 'https://identity.doordash.com'
 URL = 'https://www.doordash.com/store/yafo-kitchen-charlotte-63003'
+LOGIN_ROUTE = '/auth'
+
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+'origin':BASE_URL, 'referer': BASE_URL + LOGIN_ROUTE}
+
+s = requests.session()
+
+# csrf_token = s.get(BASE_URL).cookies['csrftoken']
+
+csrf_token = s.get(BASE_URL)
+
+login_payload = {
+    'clientId': "1666519390426295040",
+    'deviceId': '',
+    'password': "n4355342551`457314",
+    'redirectUri': "https://www.doordash.com/post-login/",
+    'responseType': "code",
+    'scope': "*",
+    'state': "/home/||5846e291-8591-427d-8c92-f8352b04e225",
+    'username': "test@gmail.com",
+    'csrfmiddlewaretoken': csrf_token,
+}
+
+login_req = s.post(BASE_URL + LOGIN_ROUTE, headers=HEADERS, data=login_payload)
+print('--*******-LoginResponse-********---',login_req)
 
 source = requests.get(URL).text
 
@@ -16,8 +41,8 @@ csv_writer = csv.writer(csv_file)
 csv_writer.writerow(["Category", "Item Name", "Description Price",	"Option Name", "Choice", "Choice Price"])
 
 
-for menus in soup.find_all('h1'):
+for menus in soup.find_all('div'):
     csv_writer.writerow([menus])
-    print(menus)
+    print('--*******-H1Body-********---', menus)
 
 # print(soup.prettify())
